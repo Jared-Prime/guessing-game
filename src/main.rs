09cf8acecae07a587ccc6f69d5ba26a2354acddc
Jared Davis <jared@haiq.us>
2015-02-08 16:12:59 -0600

    generating random numbers with std::rand
    pass type declaration u32 to the random() function

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,9 @@ use std::rand;
 fn main() {
     println!("Guess a number between 1 and 100");
 
+    // generate random unsigned 32 bit integer, take mod 100, add one
+    let secret = (rand::random::<u32>() % 100) + 1;
+
     println!("Please input your guess");
 
     let input = old_io::stdin()
