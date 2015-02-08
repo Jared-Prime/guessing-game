b6c0aafcb519ffdb7bda3fe6cde03033d36edd02
Jared Davis <jared@haiq.us>
2015-02-08 16:56:47 -0600

    loop until match found

--- src/main.rs
+++ src/main.rs
@@ -8,29 +8,34 @@ fn main() {
     // generate random unsigned 32 bit integer, take mod 100, add one
     let secret = (rand::random::<u32>() % 100) + 1;
 
-    println!("Please input your guess");
+    loop {
+        println!("Please input your guess");
 
-    let input = old_io::stdin()
-                    .read_line()
-                    .ok()
-                    .expect("Failed to read line");
+        let input = old_io::stdin()
+                        .read_line()
+                        .ok()
+                        .expect("Failed to read line");
 
-    // change our &str input to an unsigned 32 bit integer
-    let guess: Option<u32> = input.trim().parse().ok();
+        // change our &str input to an unsigned 32 bit integer
+        let guess: Option<u32> = input.trim().parse().ok();
 
-    // we need to unwrap the parsed input, verify it as an integer
-    let guess_num = match guess {
-        Some(guess) => guess,
-        None => {
-          println!("please input a number!");
-          return;
+        // we need to unwrap the parsed input, verify it as an integer
+        let guess_num = match guess {
+            Some(guess) => guess,
+            None => {
+              println!("please input a number!");
+              continue;
+            }
+        };
+
+        match cmp(secret, guess_num) {
+          Ordering::Less => println!("too small!"),
+          Ordering::Greater => println!("too big!"),
+          Ordering::Equal => {
+              println!("you win!");
+              return;
+          },
         }
-    };
-
-    match cmp(secret, guess_num) {
-      Ordering::Less => println!("too small!"),
-      Ordering::Greater => println!("too big!"),
-      Ordering::Equal => println!("you win!"),
     }
 }
 
