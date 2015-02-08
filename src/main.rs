ebb287f0e3ffa0ea162c2bfb3ab1008da6359ff5
Jared Davis <jared@haiq.us>
2015-02-08 16:11:15 -0600

    collect user input
    using std::old_io

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::old_io;
+use std::rand;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess a number between 1 and 100");
+
+    println!("Please input your guess");
+
+    let input = old_io::stdin()
+                    .read_line()
+                    .ok()
+                    .expect("Failed to read line");
+
+    println!("You guessed: {}", input);
 }
